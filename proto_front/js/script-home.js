2619b6c30fa7cda46b4d83994aebcc924ac3be09
Alan Akra <alanakra@hotmail.fr>
2020-04-28 12:24:07 +0200

    Ajout de modifications front

---
+++ proto_front/js/script-home.js
@@ -0,0 +1,36 @@
+  var lat;
+  var long;
+
+  document.getElementById("container-map").style.display = "none";
+
+  //Check if browser supports W3C Geolocation API
+  if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+  }
+  //Get latitude and longitude;
+  function successFunction(position) {
+  var lat = position.coords.latitude;
+  var long = position.coords.longitude;
+  document.getElementById("container-map").style.display = '';
+  map(lat,long);
+  }
+
+  function errorFunction(){
+  console.log("La localisation n'est pas activée sur votre appareil");
+  }
+
+  async function map(lat,long){
+
+    // Making map and tiles
+    const mymap = L.map('js-map').setView([lat, long], 15);
+  
+    const attribution = '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+  
+    const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  
+    const tiles = L.tileLayer(tileUrl, {attribution});
+    tiles.addTo(mymap);
+  
+    // Création du marker
+   L.marker([ lat , long ]).addTo(mymap);
+  }
