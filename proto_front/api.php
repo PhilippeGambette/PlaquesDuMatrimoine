8dd64eaa5556db5f01ace614202ac95dd2c005ee
Alan <alanakra@hotmail.fr>
2020-06-12 18:29:09 +0200

    Travail du 12 juin

--- proto_front/api.php
+++ proto_front/api.php
@@ -1,6 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET');
+header('content-type:application/json');
 require './include/connexion.php';
 
 // Connection to database
@@ -14,42 +15,55 @@ require './include/connexion.php';
 
 // Write mode
  if($_GET["action"] == "write"){
- // Insertion of data from Wikidata
-  var_dump($_GET);
-  $id_wikidata = $_GET['id_wikidata'];
-  $alias = $_GET['alias'];
-  $nom_complet = $_GET['nom_complet'];
-  $genderLabel = $_GET['genderLabel'];
-  $personDescription = $_GET['personDescription'];
-  $siteLink = $_GET['siteLink'];
-  $lemma = $_GET['lemma'];
 
+   // Insertion of data from Wikidata
 
-  $requete = $db->exec("INSERT INTO `personne` (`id`, `id_wikidata`, `alias`, `nom_complet`, `genderLabel`, `personDescription`, `sitelink`, `lemma`) VALUES (NULL, '".$id_wikidata. "', '". $alias . "', '". $nom_complet . "', '".$genderLabel . "', '" .$personDescription. "', '" . $siteLink . "', '". $lemma . "')") ;
+   $id_wikidata = $_GET['id_wikidata'];
+   $alias = $_GET['alias'];
+   $nom_complet = $_GET['nom_complet'];
+   $genderLabel = $_GET['genderLabel'];
+   $personDescription = $_GET['personDescription'];
+   $siteLink = $_GET['siteLink'];
+   $lemma = $_GET['lemma'];
+   
+   //  Check if potential name is in "alias" table
+   $sqlCheckAlias = "SELECT COUNT(id) AS nombre_personne FROM `personne` WHERE nom_complet =:nom_complet AND alias=:alias GROUP BY nom_complet, alias " ;
+   $requestCheckAlias = $db -> prepare($sqlCheckAlias);
+   $requestCheckAlias -> execute(array(":nom_complet" => $_GET['nom_complet'], ":alias" => $_GET['alias']));
+   
+   $data = $requestCheckAlias -> fetch();
+
+  //  Check if 1 or more name appear in the "alias" table
+   if(intval($data["nombre_personne"])>= 1){
+     
+    // If true, this message will be return
+     echo '{"message":"Alias already found in table"}';
+
+   }else{
+
+    // If false, the data are stocked in database
+     $sqlInsertWrite = "INSERT INTO `personne` (`id`, `id_wikidata`, `alias`, `nom_complet`, `genderLabel`, `personDescription`, `sitelink`, `lemma`) VALUES (NULL, :id_wikidata, :alias, :nom_complet, :genderLabel, :personDescription, :siteLink, :lemma)";
+     $requestInsertAlias = $db -> prepare($sqlInsertWrite);
+     $requestInsertAlias -> execute(array(":id_wikidata" => $_GET['id_wikidata'], ":alias" => $_GET['alias'], ":nom_complet" => $_GET['nom_complet'], ":genderLabel" => $_GET['genderLabel'], ":personDescription" => $_GET['personDescription'], ":siteLink" => $_GET['siteLink'], ":lemma" => $_GET['lemma']));
+     $db->exec();
+
+    //  And this message will be returned
+     echo '{"message": "New alias in the table"}';
+   }
 
  }
- // Read mode
+
+// Read mode
   else if($_GET["action"] == "read"){
-   var_dump($_GET);
+
    $nomPotentiel = $_GET['nom_potentiel'];
    $prenomPotentiel = $_GET['prenom_potentiel'];
 
-   $requete = "SELECT * FROM alias WHERE nom_potentiel= '$nomPotentiel' AND prenom_potentiel= '$prenomPotentiel'";
-   echo($requete);
-   $stmt = $db->prepare($requete);
-   $stmt->execute();
-   $tableauResult=$stmt->fetchAll(PDO::FETCH_ASSOC);
-   
-   $codeJSON = array();
-
-   // while($ligne = $result->fetch()){
-   //  $codeJSON[] = $ligne;
-   // }
-
-   foreach($tableauResult as $result){echo "<BR>".$result["nom_complet_potentiel"] ."<BR>";}
-   
-   // header('content-type:application/json');
-   // echo(json_encode($codeJSON, JSON_UNESCAPED_UNICODE));
+  //  Check if the name is in the database
+   $sqlCheckName = "SELECT * FROM alias WHERE nom_potentiel= :nom_potentiel AND prenom_potentiel= :prenomPotentiel";
+   $requestCheckName = $db->prepare($sqlCheckName);
+   $requestCheckName = execute(array(":nom_potentiel" => $_GET['nom_potentiel'], ":prenom_potentiel" => $_GET['prenom_potentiel']));
+  
 
  }
   /**url write mode
