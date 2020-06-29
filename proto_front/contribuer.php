b18919aab20bccf2e4be3ab8640afa0c04149626
Alan <alanakra@hotmail.fr>
2020-06-29 17:32:25 +0200

    Formulaire de contribution

--- proto_front/contribuer.php
+++ proto_front/contribuer.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,11 +8,19 @@
 </head>
 <body>
  <?php include 'include/__navbar.php' ?>
- <div class="container-soon">
-   <div class="all-soon">
-    <h1 class="title-soon">Prochainement</h1>
-     <a class="link-soon" href="index.php">Retour à l'accueil</a>
-   </div>
+  <div class="container-contribution">
+     <!-- <h1>Contribution pour la ville de </h1>  -->
+     <h1>Contribution pour <?php echo $_GET['cityname'] ?></h1> 
+     
+     <form action="sendmail.php" method="get">
+      <label for="sender">Votre adresse e-mail</label> <br>
+      <input type="email" name="sender" id=""> <br><br>
+
+      <label for="message">Informations sur la personne</label><br>
+      <textarea name="message" id="" cols="30" rows="10" placeholder=""></textarea> <br>
+
+      <input type="submit" value="Envoyer">
+     </form>
   </div>
  <?php include 'include/__footer.php' ?>
 </body>
